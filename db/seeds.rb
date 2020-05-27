06c52304beb5823a28df224e6fbc7f02a55b6388
Paulo Sacilotti <paulo.sacilotti@gmail.com>
2020-05-27 15:32:23 -0300

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Rinconcito Peruano', address: 'rua dos pinheiros', phone_number: '911111111', category: 'chinese')
+Restaurant.create(name: 'Bullguer', address: 'jardins', phone_number: '922222222', category: 'belgian')
+Restaurant.create(name: 'Gigio', address: 'rua dos pinheiros', phone_number: '933333333', category: 'italian')
+Restaurant.create(name: 'Chou', address: 'rua mateus grou', phone_number: '944444444', category: 'french')
+Restaurant.create(name: 'NKK Sushi', address: 'jardins', phone_number: '955555555', category: 'japanese')
